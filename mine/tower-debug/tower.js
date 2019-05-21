f6b4ffda118ca5aaefc799ef599c6761335f37b0
Vinson Fernandez <vinson.fernandez@admindirect.com>
2019-05-20 17:01:40 -0700

    update checklist

---
+++ mine/tower-debug/tower.js
@@ -0,0 +1,47 @@
+window.onload = startUp;
+var canvas, ctx, ball;
+
+function startUp(){
+    canvas = document.getElementById('tower-defense');
+    ctx = canvas.getContext('2d');
+    ball= new Ball();
+    canvas.style.backgroundColor = 'green';
+    resize();
+    animate();
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    ball.update();
+}
+
+
+function Ball(){
+    this.x = Math.random()*window.innerWidth;
+    this.y = Math.random()*window.innerHeight;
+    this.dx = Math.random()*6-3;
+    this.dy = Math.random()*6-3;
+    this.rad = Math.random()*10+15;
+
+    this.update = function(){
+        this.x += this.dx;
+        this.y += this.dy;
+        this.render();
+    }
+
+    this.render = function(){
+        ctx.strokeStyle = 'rgba(2,2,2,1)';
+        ctx.fillStyle = 'rgba(155,100,20,.8)';
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.rad,Math.PI*2,0,false);
+        ctx.stroke();
+        ctx.fill();
+    }
+
+}
+
+window.addEventListener('resize', resize);
+function resize() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+}
